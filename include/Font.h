00fca9b91079ad9fef2d37af304d69f9932abc7b
n2omatt <n2o.matt@gmail.com>
2016-05-17 20:14:01 -0300

    - Start the implementation of Font class.

--- include/Font.h
+++ include/Font.h
@@ -3,15 +3,26 @@
 
 //std
 #include <string>
+//Lore
+#include "Lore_Utils.h"
 
 NS_LORE_BEGIN
 
 class Font
 {
+public:
+    typedef std::shared_ptr<Font> SPtr;
+
     // CTOR //
 public:
-    Font();
+    Font(TTF_Font *font);
+    ~Font();
 
+    COREGAME_DISALLOW_COPY_MOVE_ASSIGNS(Font);
+
+    // iVars //
+private:
+    TTF_Font *m_pFont;
 };
 
 NS_LORE_END
--- src/Font.cpp
+++ src/Font.cpp
@@ -1,1 +1,21 @@
+//Lore
+#include "../include/Font.h"
+
+//Usings
+USING_NS_LORE;
+
+// CTOR //
+Font::Font(TTF_Font *font) :
+    m_pFont(font)
+{
+    //Empty...
+}
+
+Font::~Font()
+{
+    cerr << "Font DTOR" << endl;
+
+    TTF_CloseFont(m_pFont);
+    m_pFont = nullptr;
+}
 
