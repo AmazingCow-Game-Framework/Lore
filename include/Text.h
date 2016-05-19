ff5adf67f53d289316a6d4731f5a3ff6883d6269
n2omatt <n2o.matt@gmail.com>
2016-05-18 21:19:43 -0300

    - Start the implementation.

--- include/Text.h
+++ include/Text.h
@@ -5,10 +5,13 @@
 #include <string>
 //Lore
 #include "Lore_Utils.h"
+#include "IDrawable.h"
+#include "ITransformable.h"
+#include "Font.h"
 
 NS_LORE_BEGIN
 
-class Text
+class Text : public IDrawable, public ITransformable
 {
     // CTOR //
 public:
@@ -19,7 +22,7 @@ public:
     // Public Methods //
 public:
     //Draw
-    void draw();
+    virtual void draw() override;
 
     //String
     void setString(char *fmt, ...);
@@ -32,35 +35,27 @@ public:
     void setBackgroundColor(const SDL_Color &color);
     const SDL_Color& getBackgroundColor() const;
 
-    //Posiiton
-    void setPosition(const SDL_Point &position);
-    SDL_Point getPosition() const;
-
     //Font
-    void setFont(TTF_Font *font);
+    void setFont(Font::SPtr font);
     void loadFont(const std::string &filename, int size);
 
-    //Bounding Box
-    const SDL_Rect& getBoundingBox() const;
+    //Bounds
+    Rectangle getBounds() const;
 
     // Private Methods //
 private:
     void calculate();
 
-
     // iVars //
 private:
     std::string m_string;
 
-    SDL_Color   m_fgColor;
-    SDL_Color   m_bgColor;
+    Font::SPtr m_pFont;
 
-    SDL_Rect m_srcRect;
-    SDL_Rect m_dstRect;
+    SDL_Color m_fgColor;
+    SDL_Color m_bgColor;
 
-    TTF_Font   *m_pFont;
-
-    SDL_Texture *m_pTexture;
+    Vector2 m_size;
 };
 
 NS_LORE_END
--- src/Text.cpp
+++ src/Text.cpp
@@ -8,3 +8,68 @@
 
 //Usings
 USING_NS_LORE
+
+// Public Methods //
+//Draw
+void Text::draw()
+{
+
+}
+
+//String
+void Text::setString(char *fmt, ...)
+{
+    m_string = fmt;
+}
+
+const std::string& Text::getString() const
+{
+    return m_string;
+}
+
+
+//Color
+void Text::setForegroundColor(const SDL_Color &color)
+{
+    m_fgColor = color;
+}
+
+const SDL_Color& Text::getForegroundColor() const
+{
+    return m_fgColor;
+}
+
+
+void Text::setBackgroundColor(const SDL_Color &color)
+{
+    m_bgColor;
+}
+
+const SDL_Color& Text::getBackgroundColor() const
+{
+    return m_bgColor;
+}
+
+
+//Font
+void Text::setFont(Font::SPtr font)
+{
+
+}
+
+void Text::loadFont(const std::string &filename, int size)
+{
+
+}
+
+//Bounds
+Rectangle Text::getBounds() const
+{
+
+}
+
+// Private Methods //
+void Text::calculate()
+{
+
+}
