f0b9e2069f295120e8d66209ece31641a97f67d5
n2omatt <n2o.matt@gmail.com>
2016-05-24 02:43:51 -0300

    - Add the color class.

---
+++ include/Color.h
@@ -0,0 +1,35 @@
+#ifndef __Lore_include_Color_h__
+#define __Lore_include_Color_h__
+
+//std
+#include <string>
+//Lore
+#include "Lore_Utils.h"
+
+NS_LORE_BEGIN
+
+class Color
+{
+    // Static Methods //
+public:
+    static const Color& White      ();
+    static const Color& Black      ();
+    static const Color& Transparent();
+
+
+    // CTOR //
+public:
+    Color();
+    Color(int red, int green, int blue, int alpha = 255);
+
+
+    // iVars //
+public:
+    int r, g, b, a;
+
+};
+
+NS_LORE_END
+#endif // __Lore_include_Color_h__ //
+
+
---
+++ src/Color.cpp
@@ -0,0 +1,42 @@
+//Header
+#include "../include/Color.h"
+
+//Usings
+USING_NS_LORE
+
+// Static Methods //
+const Color& Color::White()
+{
+    static Color s_white;
+    return s_white;
+}
+
+const Color& Color::Black()
+{
+    static Color s_black(0, 0, 0);
+    return s_black;
+}
+
+const Color& Color::Transparent()
+{
+    static Color s_transparent(0, 0, 0, 0);
+    return s_transparent;
+}
+
+
+// CTOR //
+Color::Color() :
+    Color(255, 255, 255, 255) // White //
+{
+    //Empty...
+}
+
+Color::Color(int red, int green, int blue, int alpha /* = 255 */) :
+    r(red),
+    g(green),
+    b(blue),
+    a(alpha)
+{
+    //Empty...
+}
+
