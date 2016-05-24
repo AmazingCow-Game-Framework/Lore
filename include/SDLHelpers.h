a0f4945935f03aa170a0ec3cefe576d0bc806824
n2omatt <n2o.matt@gmail.com>
2016-05-24 02:49:15 -0300

    - Add a lot of new helper methods. - Add the comments separators. - Fix the dammit bug on load_font_from_file.

--- include/SDLHelpers.h
+++ include/SDLHelpers.h
@@ -5,25 +5,37 @@
 #include <string>
 //Lore
 #include "Lore_Utils.h"
+#include "Color.h"
+#include "Rectangle.h"
+#include "Vector2.h"
 
 
 NS_LORE_BEGIN
-
 namespace SDLHelpers {
 
-// Some SDL Helpers //
-SDL_Point    make_point(int x, int y);
-SDL_Rect     make_rect(int x, int y, int w, int h);
-SDL_Color    make_color(int r, int g, int b, int a = 255);
+// Point //
+SDL_Point make_point(int x, int y);
+SDL_Point make_point(const Vector2 &vector);
+SDL_Point make_point(const Rectangle &rect);
 
-//Texture
+// Rectangle //
+SDL_Rect make_rect(int x, int y, int w, int h);
+SDL_Rect make_rect(const Vector2 &pos, const Vector2 &size);
+SDL_Rect make_rect(const Rectangle &rect);
+
+// Color //
+SDL_Color make_color(int r, int g, int b, int a = 255);
+SDL_Color make_color(const Color &color);
+
+// Texture //
 SDL_Texture* load_texture_from_file(const std::string &filename);
 SDL_Texture* make_texture_free(SDL_Surface *surface);
+SDL_Texture* make_texture_empty(int width = 0, int height = 0);
 
-//Font
+// Font //
 TTF_Font* load_font_from_file(const std::string &filename, int size);
 
-}; //namespace SDLHelpers
 
+}; //namespace SDLHelpers
 NS_LORE_END
 #endif // __Lore_include_SDLHelpers_h__ //
--- src/SDLHelpers.cpp
+++ src/SDLHelpers.cpp
@@ -9,22 +9,62 @@ USING_NS_LORE
 
 NS_LORE_BEGIN
 
+////////////////////////////////////////////////////////////////////////////////
+// Point                                                                      //
+////////////////////////////////////////////////////////////////////////////////
 SDL_Point SDLHelpers::make_point(int x, int y)
 {
     return (SDL_Point){x, y};
 }
 
+SDL_Point SDLHelpers::make_point(const Vector2 &vector)
+{
+    return make_point(vector.x, vector.y);
+}
+
+SDL_Point SDLHelpers::make_point(const Rectangle &rect)
+{
+    return make_point(rect.getLocation());
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Rectangle                                                                  //
+////////////////////////////////////////////////////////////////////////////////
 SDL_Rect SDLHelpers::make_rect(int x, int y, int w, int h)
 {
     return (SDL_Rect){x, y, w, h};
 }
 
+SDL_Rect SDLHelpers::make_rect(const Vector2 &pos, const Vector2 &size)
+{
+    return make_rect(pos.x, pos.y, size.x, size.y);
+}
+
+SDL_Rect SDLHelpers::make_rect(const Rectangle &rect)
+{
+    return make_rect(rect.getLocation(), rect.getSize());
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Color                                                                      //
+////////////////////////////////////////////////////////////////////////////////
 SDL_Color SDLHelpers::make_color(int r, int g, int b, int a /* = 255 */)
 {
     //COWTODO: Check errors.
     return (SDL_Color){r, g, b, a};
 }
 
+SDL_Color SDLHelpers::make_color(const Color &color)
+{
+    return make_color(color.r, color.g, color.b, color.a);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Texture                                                                    //
+////////////////////////////////////////////////////////////////////////////////
 SDL_Texture* SDLHelpers::load_texture_from_file(const std::string &filename)
 {
     auto surface = IMG_Load(filename.c_str());
@@ -51,11 +91,27 @@ SDL_Texture* SDLHelpers::make_texture_free(SDL_Surface *surface)
 
     return texture;
 }
+SDL_Texture* SDLHelpers::make_texture_empty(int width  /* = 0 */,
+                                            int height /* = 0 */)
+{
+    //COWTODO: Check if this is the correct way to get             \
+    //         an empty texture. i.e. We shouldn't check           \
+    //         the renderer first to get the pixel format and all?
+    auto renderer = WindowManager::instance()->getRenderer();
+    auto texture = SDL_CreateTexture(renderer,
+                                     SDL_PIXELFORMAT_RGBA8888,
+                                     SDL_TEXTUREACCESS_STATIC,
+                                     width, height);
 
-//Font
+    return texture;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Font                                                                       //
+////////////////////////////////////////////////////////////////////////////////
 TTF_Font* SDLHelpers::load_font_from_file(const std::string &filename, int size)
 {
-    auto font = TTF_OpenFont(filename.c_str(), 16);
+    auto font = TTF_OpenFont(filename.c_str(), size);
 
     COREGAME_ASSERT_ARGS(font != nullptr,
                          "Cannot load font with filename - %s - size - %d",
