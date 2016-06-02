f35c06c35274e165c706d5698d0c23a8e342a03a
n2omatt <n2o.matt@gmail.com>
2016-06-02 11:10:09 -0300

    - Add the private utils.

---
+++ src/private/include/Lore_Private_Utils.h
@@ -0,0 +1,9 @@
+#ifndef __Lore_src_private_include_Lore_Private_Utils_h__
+#define __Lore_src_private_include_Lore_Private_Utils_h__
+
+#define NS_LORE_PVT_BEGIN namespace Lore_Private {
+#define NS_LORE_PVT_END   }
+#define USING_NS_LORE_PVT using namespace Lore_Private;
+
+
+#endif // __Lore_src_private_include_Lore_Private_Utils_h__ //
