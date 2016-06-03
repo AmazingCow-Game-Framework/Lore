ab2633e062c305ffdea75de8518b3309a3463d59
n2omatt <n2o.matt@gmail.com>
2016-06-02 21:10:33 -0300

    [WIP]

--- src/Font.cpp
+++ src/Font.cpp
@@ -32,10 +32,15 @@ Texture::SPtr Font::prepareTexture(const std::string &str,
         return std::make_shared<Texture>(SDLHelpers::make_texture_empty());
 
 
-    auto tmp_surface = TTF_RenderText_Shaded(m_pFont,
+    auto tmp_surface = TTF_RenderText_Solid(m_pFont,
                                              str.c_str(),
-                                             SDLHelpers::make_color(fgColor),
-                                             SDLHelpers::make_color(bgColor));
+                                             SDLHelpers::make_color(fgColor));
+                                             //{0xff, 0x0, 0xff, 0x05});
+                                             //SDLHelpers::make_color(bgColor));
+    SDL_SetSurfaceBlendMode(tmp_surface, SDL_BLENDMODE_BLEND);
 
-    return std::make_shared<Texture>(SDLHelpers::make_texture_free(tmp_surface));
+    auto sdl_texture = SDLHelpers::make_texture_free(tmp_surface);
+    SDL_SetTextureBlendMode(sdl_texture, SDL_BLENDMODE_BLEND);
+
+    return std::make_shared<Texture>(sdl_texture);
 }
